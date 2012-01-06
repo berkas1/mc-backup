8c73952fc28c4db80c03865bce781e52189b0d10
berkas1 <berka@berkasimon.com>
2012-01-06 09:48:40 +0100

    added comments

--- backup.sh
+++ backup.sh
@@ -1,21 +1,25 @@
-directory=`date +%Y-%m-%d`;
-
+directory_new=`date +%Y-%m-%d`;
+directory_source=/home/berkas1/ms  #source directory - the directory where is your server
 
 
 
 function create-directory {
-if [ -d "$directory" ]; then
+if [ -d "$directory_new" ]; then #check if directory already exist or not
 
-        echo "backup already exists!";
+        echo "Backup already exists!";
         exit 0
 else
-        mkdir $directory;
-        cp -r /home/berkas1/ms $directory/;
-        echo "mc was backed up";
+        mkdir $directory_new;
+        echo "Directory was created successfuly!";
 fi
+}
+
+function backup-server {
+cp -r $directory_source $directory_new/;
+echo "Your minecraft server was backed up successfuly";
+#copy the server folder to target location
 
 }
 
-
-
 create-directory;
+backup-server;
