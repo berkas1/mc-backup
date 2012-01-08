73b8ea1fd11d58bc5ec2c59580d8775f8553cbb8
berkas1 <berka@berkasimon.com>
2012-01-08 15:18:03 +0100

    full vs working - alpha version

--- backup.sh
+++ backup.sh
@@ -2,14 +2,14 @@
 
 #!/bin/bash
 datet=`date +%Y-%m-%d`;
-director="`date +%Y-%m-%d`-*";
+director=`date +%Y-%m-%d-*`;
 directory_source=/home/berkas1/ms;  #source directory - the directory where is your server
-directory_last=`ls -1 | find 2012-01-07-* | tail -1`;
+last_backup=`ls | grep "$director" | tail -1`;
 
 
 function find-directory {
-if [ -d "$datet-*" ];then
-	echo "exist";
+if [ -d "$datet-1" ];then
+	echo "Backup already exist, creating new one.";
 	#next-function
 else 
 	mkdir $datet"-1";
@@ -19,39 +19,29 @@ else
 fi
 }
 
-function create-next-directory {
-
-
-
+function find-next-directory {
+lastbp="${last_backup##*-}";
+newbackup="$lastbp";
+newbackup=$(($newbackup + 1));
+create-next-directory;
 }
 
+function create-next-directory {
+newdir=$datet"-"$newbackup;
+mkdir $newdir;
+}
 
-function create-directory {
-if [ -d "$directory_new" ]; then #check if directory already exist or not
-
-        echo "Backup already exists!";
-        exit 0
-else
-        mkdir $directory_new;
-        echo "Directory was created successfuly!";
-fi
+function backup-server-next {
+cp -r $directory_source $newdir/; 
+echo "Your server was successfuly backed up into $newdir directory"
 }
 
 function backup-server {
-cp -r $directory_source $directory_new/;
+cp -r $directory_source $datet"-1"/;
 echo "Your minecraft server was backed up successfuly";
 #copy the server folder to target location
 
 }
-
-#create-directory;
-#backup-server;
-
-#create-directory-once;
-#echo $director
-#echo $datet
-
-#ls | grep $director
-
-#echo $directory_last;
-#mkdir $datet"-1";
+find-directory
+find-next-directory
+backup-server-next
